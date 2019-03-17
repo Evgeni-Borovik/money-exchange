84e59ebd5d05f9cc42aac5f163f3cf6b3692e8d5
Evgeni-Borovik <anyuser1980@gmail.com>
2019-03-18 00:34:44 +0300

    feature: add 1st step of calculation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
+// module.exports = function makeExchange(currency) {
+    var exchange = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    var coins = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+    var amount = currency;
+    var output = {};
+    for (var key in coins) {
+        // этот код будет вызван для каждого свойства объекта
+        var div_result = Math.floor(amount / coins[key]);
+        if (div_result > 0) {
+            amount = amount - coins[key] * div_result;
+            output[key] = div_result;
+            console.log(output);
+        }
+    }
 }
+
+exchange(100);
